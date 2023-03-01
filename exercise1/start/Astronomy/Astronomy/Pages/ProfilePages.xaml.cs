4947bbffcec18244d5ff4fc639d1ec48822052c4
teukuauli <teukuauli18@gmail.com>
2023-03-02 00:42:56 +0700

    first commit

---
+++ exercise1/start/Astronomy/Astronomy/Pages/ProfilePages.xaml.cs
@@ -0,0 +1,9 @@
+namespace Astronomy.Pages;
+
+public partial class ProfilePages : ContentPage
+{
+	public ProfilePages()
+	{
+		InitializeComponent();
+	}
+}
---
+++ exercise3/start/Astronomy/Astronomy/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Astronomy.Pages;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
